46657fff06deb5d79e40d2292e074c49ef282a2f
mohamed abdelghafour <touari.med0abdelghafour@gmail.com>
2026-04-18 14:00:44 +0100

    Update createUsers_pub.ps1
    added account description

--- createUsers_pub.ps1
+++ createUsers_pub.ps1
@@ -1,24 +1,33 @@
+# Define the accounts and their specific descriptions
+$Accounts = @{
+    "repl_snapshot"     = "SQL Replication Snapshot Agent Account"
+    "repl_logreader"    = "SQL Replication Log Reader Agent Account"
+    "repl_distribution" = "SQL Replication Distribution Agent Account"
+    "repl_merge"        = "SQL Replication Merge Agent Account"
+}
+
 $Password = "Poste@2025"
 $Computer = [ADSI]"WinNT://$env:COMPUTERNAME"
-$AccountNames = "repl_snapshot","repl_logreader","repl_distribution","repl_merge"
 
-foreach ($Name in $AccountNames) {
+foreach ($Name in $Accounts.Keys) {
+    $Description = $Accounts[$Name]
+    
     try {
-        # Create the user
+        # Create the user object
         $User = $Computer.Create("User", $Name)
         $User.SetPassword($Password)
         $User.Put("FullName", $Name)
-        $User.Put("Description", "SQL Replication Account")
+        $User.Put("Description", $Description)
         $User.SetInfo()
 
-        # Set Password Never Expires (Flag 0x10000)
-        $Flag = $User.Properties.userFlags.Value -bor 0x10000
-        $User.Properties.userFlags.Value = $Flag
+        # Set 'Password Never Expires' (Flag 0x10000)
+        $User.userFlags = $User.userFlags.Value -bor 0x10000
         $User.SetInfo()
 
-        Write-Host "Successfully created: $Name" -ForegroundColor Green
+        Write-Host "Successfully created $Name with description: '$Description'" -ForegroundColor Green
     }
     catch {
-        Write-Host "Failed to create $Name : $($_.Exception.Message)" -ForegroundColor Red
+        Write-Host "Error creating $Name : $($_.Exception.Message)" -ForegroundColor Red
+ 
     }
 }
