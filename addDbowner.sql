bd9956aa02567818de983f4f1e8c0c61275fe4a0
mohamed abdelghafour <touari.med0abdelghafour@gmail.com>
2026-04-19 11:28:23 +0100

    Create addDbowner.sql

---
+++ addDbowner.sql
@@ -0,0 +1,62 @@
+USE [master];
+GO
+
+-- =========================
+-- PARAMETERS
+-- =========================
+DECLARE @prefix NVARCHAR(128) = @@SERVERNAME;
+-- SET @prefix = 'MYDOMAIN'; -- if needed
+
+DECLARE @databases TABLE (db SYSNAME);
+INSERT INTO @databases VALUES
+('distribution'),
+('ReplicationTestDB');
+
+DECLARE @accounts TABLE (name SYSNAME);
+INSERT INTO @accounts VALUES
+('repl_snapshot'),
+('repl_logreader'),
+('repl_distribution'),
+('repl_merge');
+
+-- =========================
+-- PROCESS
+-- =========================
+DECLARE @sql NVARCHAR(MAX) = N'';
+
+SELECT @sql = @sql + '
+USE ' + QUOTENAME(d.db) + ';
+PRINT ''--- Processing DB: ' + d.db + ' ---'';
+
+' + (
+    SELECT STRING_AGG(
+'
+IF NOT EXISTS (
+    SELECT 1 FROM sys.database_principals 
+    WHERE name = ''' + @prefix + '\' + a.name + '''
+)
+BEGIN
+    CREATE USER ' + QUOTENAME(@prefix + '\' + a.name) + ' 
+    FOR LOGIN ' + QUOTENAME(@prefix + '\' + a.name) + ';
+    PRINT ''User created: ' + @prefix + '\' + a.name + ''';
+END
+
+BEGIN TRY
+    ALTER ROLE db_owner ADD MEMBER ' + QUOTENAME(@prefix + '\' + a.name) + ';
+    PRINT ''Added to db_owner: ' + @prefix + '\' + a.name + ''';
+END TRY
+BEGIN CATCH
+    PRINT ''Already in db_owner: ' + @prefix + '\' + a.name + ''';
+END CATCH
+'
+, CHAR(10))
+    FROM @accounts a
+) + '
+'
+FROM @databases d;
+
+-- =========================
+-- EXECUTION
+-- =========================
+EXEC sp_executesql @sql;
+GO
